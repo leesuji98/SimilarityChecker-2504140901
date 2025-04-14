914c8107bb480fad7828ab7959381e0531ace66e
leesuji <leesuji98@naver.com>
2025-04-14 09:37:06 +0900

    same length point

--- Project25/main.cpp
+++ Project25/main.cpp
@@ -1,6 +1,14 @@
 #include "gmock/gmock.h"
 #include "stringChecker.cpp"
 
+TEST(TS, TC1) {
+	StringChecker stringChecker;
+	stringChecker.setString("ASD", "DSA");
+	int actual = stringChecker.pointOfLength();
+	int expected = 60;
+	EXPECT_EQ(expected, actual);
+}
+
 int main()
 {
 	::testing::InitGoogleMock();
--- Project25/stringChecker.cpp
+++ Project25/stringChecker.cpp
@@ -1,5 +1,20 @@
-
+#include <string>
+using std::string;
 
 class StringChecker {
+private:
+	string s1;
+	string s2;
 public:
+	void setString(string a, string b) {
+		s1 = a, s2 = b;
+	}
+
+	bool isSameLength() const {
+		return s1.length() == s2.length();
+	}
+
+	int pointOfLength() {
+		if (isSameLength()) return 60;
+	}
 };
