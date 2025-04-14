d843c81fbf767227fda4d3acca2cb2454fba70ec
leesuji <leesuji98@naver.com>
2025-04-14 10:46:28 +0900

    alpha check

--- Project25/main.cpp
+++ Project25/main.cpp
@@ -10,6 +10,12 @@ public:
 		int actual = stringChecker.pointOfLength();
 		EXPECT_EQ(expected, actual);
 	}
+
+	void alphaChecker(int expected, string s1, string s2) {
+		stringChecker.setString(s1, s2);
+		int actual = stringChecker.pointOfAlpha();
+		EXPECT_EQ(expected, actual);
+	}
 };
 
 TEST_F(SimilarityCheckerFixture, TC1) {
@@ -26,6 +32,22 @@ TEST_F(SimilarityCheckerFixture, TC3) {
 
 TEST_F(SimilarityCheckerFixture, TC4) {
 	lengthChecker(30, "AA", "AAE");
+}
+
+TEST_F(SimilarityCheckerFixture, TC5) {
+	alphaChecker(40, "ASD", "DSA");
+}
+
+TEST_F(SimilarityCheckerFixture, TC6) {
+	alphaChecker(0, "A", "BB");
+}
+
+TEST_F(SimilarityCheckerFixture, TC7) {
+	alphaChecker(40, "AAABB", "BAA");
+}
+
+TEST_F(SimilarityCheckerFixture, TC8) {
+	alphaChecker(20, "AA", "AAE");
 }
 
 int main()
